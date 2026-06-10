2a0604ddf25a2bbc18eed6c4ae5e7eb801b7d279
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-06-10 14:28:42 +0600

    Add footer component with app promotion, links, social icons, and contact information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/CommonComponents/NavBar/NavBar";
+import Footer from "@/components/CommonComponents/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +27,13 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`  }
     >
       <body className="min-h-full flex flex-col bg-background">
         <NavBar />
-        {children}</body>
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }
--- src/components/CommonComponents/Footer/Footer.tsx
+++ src/components/CommonComponents/Footer/Footer.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import FooterAppSection from "./FooterAppSection";
+import FooterLinksSection from "./FooterLinkSection";
+import FooterSocialSection from "./FooterSocialSection";
+import FooterContactSection from "./FooterContactSection";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="w-full font-sans">
+      {/* App promotion banner */}
+      <FooterAppSection />
 
-export default Footer
+      {/* Divider */}
+      <div className="w-full h-px bg-[#EFEFEF]" />
+
+      {/* Navigation link columns */}
+      <FooterLinksSection />
+
+      {/* Payment methods + social icons + email subscribe */}
+      <FooterSocialSection />
+
+      {/* Bottom contact bar */}
+      <FooterContactSection />
+    </footer>
+  );
+};
+
+export default Footer;
